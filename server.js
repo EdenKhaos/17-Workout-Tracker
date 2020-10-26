70c6a43420d58a692605f1e4fa113a37beb4bdcc
Heather Cooper <raven.dantes@ymail.com>
2020-10-25 22:57:24 -0600

    ready for heroku

--- server.js
+++ server.js
@@ -5,18 +5,24 @@ const mongoose = require("mongoose");
 //set port
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 //use logger/morgan
 app.use(morgan("dev"));
 //parser
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
-//use static files
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+//mongoDB connection
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/17-workout-tracker";
 mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+mongoose.connection.on("connected", () => {
+    console.log("Mongoose is connected!!!!");
+});
 //use routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
